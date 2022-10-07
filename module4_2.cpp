d8fb9b4ca11673b03a964e85ebda653de67d2408
MOHSIN RATHOD <110963546+Mohsin5415@users.noreply.github.com>
2022-10-07 18:13:11 +0530

    Add files via upload

---
+++ module4_2.cpp
@@ -0,0 +1,18 @@
+
+ What is OOP? List OOP concepts
+
+===>OOP stand for object-oriented programming.
+Procedural programming is about writing procedure or funcations that perform operations on the data,
+while object oriented programming is about creating objects that contain both data and functions
+
+==>The structure of oop : The structure, or buliding blocks,of object oriented programming include the following
+1) CLASSES
+2) OBJECT
+3) METHODS
+4) ATTRIBUTES
+
+===>The main principles of oop
+1) ENCAPSULATION
+2) ABSTRACTION
+3) INHERITANCE
+4) POLYMORPHISM 
---
+++ module4_3.cpp
@@ -0,0 +1,31 @@
+What is the difference between OOP and POP?
+
+==> Basic Definition :	OOP is object-oriented OR POP is structure or procedure-oriented.
+
+===> Program Division :	OOP is divided into objects OR	POP is divided into functions.
+
+===>Approach : OOP is Bottom-Up approach OR POP Is	Top-down approach
+
+===>Data Control : Data in OOP each object is controlled on its own. OR IN POP Every function has different data, so there’s no control over it.
+
+===>Entity Linkage : OOP In Object functions are linked through message passing. OR In POP Parts of a program are linked through parameter passing.
+
+==>Expansion : IN OOP Adding new data and functions is easy  OR IN POP Expanding data and function is not easy.
+
+===>Inheritance : IN OOP	Inheritance is supported in three modes: public, private & protected  OR IN POP Inheritance is not supported.
+
+===>Access control : IN OOP Access control is done with access modifiers OR IN POP No access modifiers supported.
+
+===>Data Hiding : IN OOP Data can be hidden using Encapsulation No data hiding OR IN POP Data is accessible globally.
+
+===>Overloading or Polymorphism : IN OOP Overloading functions, constructors, and operators are done OR IN POP Overloading is not possible.
+
+===>Friend function : IN OOP Classes or functions can be linked using the keyword “friend, only in C++. OR IN POP No friend function.
+
+==>Virtual classes or functions : IN OOP The virtual function appears during inheritance OR IN POP No virtual classes or functions.
+
+===>Code Reusability : IN OOP The existing code can be reused. OR IN POP No code reusability.
+
+===>Problem Solving : OOP solving big problems  OR POP	Not suitable for solving big problems.
+
+===>Example : OOP-	C++, JAVA, VB.NET, C#.NET. OR  POP- C, VB, FORTRAN, Pascal
